e9dcf8ad5b260edea8709ecba3665a2c87023294
Adithyanstyle1 <adithyan18fm@gmail.com>
2024-04-15 12:57:22 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	const totalTickets = 50
+	var availableTickets = 50
+
+	fmt.Printf("Total %v number %v  ", totalTickets, availableTickets)
+	fmt.Println("available tickets ", availableTickets)
+
+}
